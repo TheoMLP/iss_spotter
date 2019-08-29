93f7126734e523958dbd8d432d3544fecf29903a
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-29 20:38:52 +0000

    finished fetchISSFlyOverTImes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ fetchMyIp((error, ip) => {
   error ? console.log("It didn't work", error) : console.log("Returned ip:", ip);
 });
 
-fetchCoordsByIP("67.71.216.6", (error, data) => {
-  error ? console.log("It didn't work", error) : console.log("Returned coordinates:", data);
+fetchCoordsByIP("67.71.216.6", (error, coordinates) => {
+  error ? console.log("It didn't work", error) : console.log("Returned coordinates:", coordinates);
 });
 
-fetchISSFlyOverTimes({ latitude: '43.63190', longitude: '-79.37160' }, (error, data) => {
-  error ? console.log("It didn't work", error) : console.log("Returned fly over times:", data);
+fetchISSFlyOverTimes({ latitude: '43.63190', longitude: '-79.37160' }, (error, passTimes) => {
+  error ? console.log("It didn't work", error) : console.log("Returned fly over times:", passTimes);
 });
